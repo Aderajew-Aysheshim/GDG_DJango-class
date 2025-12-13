55a06db6c7c67300c99c07850fdf48504f83dde4
eteh64 <aysheshimaderajew600@gmail.com>
2025-12-13 10:29:27 +0300

    week3task

---
+++ Desktop/GDG/week3tasks/q4rectangle.py
@@ -0,0 +1,9 @@
+class Rectangle:
+  def __init__(self,width,hight):
+    self.width=width
+    self.hight=hight
+
+  
+rect1=Rectangle(5,10)   
+print("Area of Rectangle:",rect1.width*rect1.hight)
+
