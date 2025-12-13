f69b28282d923d4f485ff2e701dabe1c05e9931f
eteh64 <aysheshimaderajew600@gmail.com>
2025-12-13 13:49:13 +0300

    Add question statements in files as code (no comments)

--- Q1_discount.py
+++ Q1_discount.py
@@ -1,9 +1,18 @@
 
+__question__ = """
+Python Practice Question 1:
+A shop gives a discount if a customer buys more than 3 items.
+Write a program that asks the user for the number of items they want
+to buy and prints:
+"Discount applied" if items > 3
+"No discount" otherwise
+"""
+
 items = int(input("Enter the number of items you want to buy: "))
 def discount(items):
- if items > 3:
-    print("Discount applied")
- else:
-    print("No discount")
+   if items > 3:
+      print("Discount applied")
+   else:
+      print("No discount")
     
     discount(items)
--- Q6salsetext.py
+++ Q6salsetext.py
@@ -1,13 +1,35 @@
+__question__ = """
+Python Practice Question 6:
+Write a program that:
+a, Reads every line in sales.txt
+b, Converts valid lines into integers
+c, Skips invalid lines (like "abc") using exception handling
+d, Stores the valid numbers in a list
+e, Calculates and prints the total sales
+"""
+
 valid_sales = []
 
 with open("sales.txt", "r") as file:
     for line in file:
         try:
-            number = int(line.strip())   # convert to integer
-            valid_sales.append(number)   # store valid number
+            number = int(line.strip())  # convert to integer
+            valid_sales.append(number)  # store valid number
         except ValueError:
-           
-           continue
+            continue
+
+print("Valid sales:", valid_sales)
+print("Total sales =", sum(valid_sales))
+valid_sales = []
+
+with open("sales.txt", "r") as file:
+    for line in file:
+        try:
+            number = int(line.strip())  # convert to integer
+            valid_sales.append(number)  # store valid number
+        except ValueError:
+
+            continue
 
 print("Valid sales:", valid_sales)
 print("Total sales =", sum(valid_sales))
